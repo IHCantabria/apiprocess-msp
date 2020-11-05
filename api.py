c9810e32c0f5ff6943e2da6920abb38e95e8b382
Felipe Maza <felipe@felipem.com>
2020-11-05 08:57:21 +0100

    added files for apache

---
+++ api.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from flask_restplus import Api, Resource, fields
+import apiprocessMSP
+from apiprocessMSP import tools
+from flask_cors import CORS
+
+
+app = Flask(__name__, instance_relative_config=True)
+CORS(app)
+apiflask = Api(app, version=apiprocessMSP.__version__, title="ApiProcess MSP")
+
+
+def create():
+    tools.start(apiflask)
+    return app
+
+
+if __name__ == "apimsp":
+    create()
